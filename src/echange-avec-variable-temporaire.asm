b7fdcc08b6cb87df29563864ac1d84ceb81aeaef
Bruno BEAUFILS <bruno@boulgour.com>
2021-04-14 19:55:37 +0200

    src: modification échange variables

--- src/echange-avec-variable-temporaire.asm
+++ src/echange-avec-variable-temporaire.asm
@@ -1,10 +1,10 @@
 ; Échange avec variable temporaire (case 12) du contenu des cases 10 et 11
 ; Idée exo : Jean-Marie PLACE
-LDA 10
-MOV A R
+LDA 10                          ; Ici utiliser une variable demande de la
+MOV A R                         ; charger de la mémoire vers un registre
 STR 12
-LDB 11
-MOV B R 
+LDA 11
+MOV A R 
 STR 10
 LDA 12
 MOV A R 
@@ -12,3 +12,11 @@ STR 11
 JMP 99
     4                           ; Variable 1
     8                           ; Variable 2
+    0                           ; Variable temporaire
+LDA 10                          ; Ici chaque variable est chargée dans un registre
+LDB 11
+MOV A R
+STR 11
+MOV B R
+STR 10
+JMP 99
